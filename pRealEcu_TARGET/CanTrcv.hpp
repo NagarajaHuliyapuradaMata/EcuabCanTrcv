b60986e7ce73b51b6abecfb5df62bafa45fd986e
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-16 08:07:38 +0200

    AUTOSAR - Hotfixes - pRealEcu_cR_TARGET - EcuabCanTrcv

---
+++ pRealEcu_TARGET/CanTrcv.hpp
@@ -0,0 +1,11 @@
+#ifndef _CANTRCV_H
+#define _CANTRCV_H
+
+#include "Can_GeneralTypes.hpp"
+
+extern void TRCV_Standby(void);
+extern void TRCV_Normal(void);
+extern boolean TRCV_GetWakeupFlag(void);
+
+#endif
+

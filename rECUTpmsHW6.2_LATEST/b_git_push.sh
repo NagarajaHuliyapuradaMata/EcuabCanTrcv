faf0d1b8c6fdcd419ce6c25f458a7dab44e07904
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2024-02-21 17:02:05 +0100

    Update

--- rECUTpmsHW6.2_LATEST/b_git_push.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git status
-git commit -m "AUTOSAR Migration Activities - rEcu_vBcRsBpTpms_LATEST - TrcvEcuabCan"
-git push
--- rECUTpmsHW6.2_TARGET/b_git_push.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git status
-git commit -m "AUTOSAR Migration Activities - rEcu_vBcRsBpTpms_TARGET - EcuabTrcvCan"
-git push
